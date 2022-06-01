dd47a0d7e7cfc39feafd32975e31be633861f1bf
nanirama <nani.rama1982@gmail.com>
2022-06-01 17:20:08 +0530

    CTA target change5

--- pages/_app.js
+++ pages/_app.js
@@ -14,16 +14,9 @@ const MyApp = ({ Component, pageProps }) => {
   const { global } = pageProps
 
   return (
-    <>
-    
-      <Head>
-        {/* <link rel="shortcut icon" href={getStrapiMedia(global.favicon)} /> */}
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" /> 
-      </Head>
       <GlobalContext.Provider>
         <Component {...pageProps} />
       </GlobalContext.Provider>
-    </>
   )
 }
 export default MyApp
