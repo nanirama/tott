27be6dab81633fbcec090203ebe3af67c74f8df1
nanirama <nani.rama1982@gmail.com>
2022-06-10 08:38:15 +0530

    loader test7

--- components/Image.js
+++ components/Image.js
@@ -5,13 +5,19 @@ const cloudflareImageLoader = ({ src, width, height, quality }) => {
   if (!quality) {
     quality = 75
   }
-  return `http://images.trady.workers.dev/?width=${width}&height=${height}&quality=${quality}&image=http://trady.dev${src}`
+  console.log('Image src',src)
+  return `http://images.trady.workers.dev/?width=${width}&height=${height}&quality=${quality}&image=${process.env.DOMAIN_URL}${src}`
 }
 
+// export default function ImgLoader(props) {
+//   if (process.env.NODE_ENV === 'development') {
+//     console.log('Image src Local',props.src)
+//     return <Image unoptimized={true} {...props} width={props.width} height={props.height} itemProp="image" alt={props.alt ? props.alt : 'Tott'} />
+//   } else {
+//     return <Image {...props} loader={cloudflareImageLoader} width={props.width} height={props.height} itemProp="image" alt={props.alt ? props.alt : 'Tott'} />
+//   }
+// }
+
 export default function ImgLoader(props) {
-  if (process.env.NODE_ENV === 'development') {
-    return <Image unoptimized={true} {...props} width={props.width} height={props.height} itemProp="image" alt={props.alt ? props.alt : 'Tott'} />
-  } else {
-    return <Image {...props} loader={cloudflareImageLoader} width={props.width} height={props.height} itemProp="image" alt={props.alt ? props.alt : 'Tott'} />
-  }
+  return <Image {...props} loader={cloudflareImageLoader} width={props.width} height={props.height} itemProp="image" alt={props.alt ? props.alt : 'Tott'} />
 }
